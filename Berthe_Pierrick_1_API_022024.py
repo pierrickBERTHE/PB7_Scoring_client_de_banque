f3a964ca9f65e1a131c26bbd492aa71c93cf2afe
Pierrick BERTHE <pierrick.berthe@gmx.fr>
2024-03-15 16:00:18 +0100

    PB_15/03 : api et dashboard parvient a faire prediction pour 1 seul client OK

--- Berthe_Pierrick_1_API_022024.py
+++ Berthe_Pierrick_1_API_022024.py
@@ -2,45 +2,59 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
-import mlflow.pyfunc
+import os
 
 # Création de l'application Flask
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-# # Chargement du modèle pré-entraîné
-# MODEL_PATH = 'best_model.joblib'
-# model = joblib.load(MODEL_PATH)
+# Récupération du chemin absolu et du répertoire du fichier api.py
+abspath = os.path.abspath(__file__)
+dirname = os.path.dirname(abspath)
 
-# Chargement du modèle pré-entraîné depuis le registre de modèles MLflow
-MODEL_NAME = "model_light_gbm_best "
-model = mlflow.pyfunc.load_model(model_uri=f"models:/{MODEL_NAME}/production")
+# Chargement du modèle pré-entraîné
+MODEL_PATH = 'mlflow_model/model.pkl'
+model = joblib.load(MODEL_PATH)
 
+# Définition de la route de la page d'accueil
+@app.route('/', methods=['GET'])
+def home():
+    description = f'''<h1>test de l'API de Pierrick</h1>
+                    <p>abspath: {abspath}</p>
+                    <p>dirname: {dirname}</p>'''
+    return description
 
-# Définition de la route pour la prédiction
+# Définition de la route de test de l'état de l'API
+@app.route('/health', methods=['GET'])
+def health():
+    """
+    Retourne un message indiquant que le serveur est opérationnel.
+    """
+    return jsonify({'status': 'ca maRche!'})
+
+# Définition de la route de prédiction
 @app.route('/predict', methods=['POST'])
 def predict():
     """
     Prédit la sortie en fonction des données d'entrée en utilisant le modèle
     pré-entraîné. Les données d'entrée sont reçues au format JSON.
     """
+    print('request:')
     try:
         # Récupération des données au format JSON
-        data_json = request.get_json()
+        data = request.get_json()
 
-        # Transformation des données JSON en DataFrame pandas
-        data = pd.DataFrame(...)  
+        # Convertir les données en DataFrame Pandas
+        df = pd.DataFrame(data)
 
         # Prédiction à l'aide du modèle pré-entraîné
-        predictions = model.predict(data)
+        predictions = model.predict(df)
 
         # Retour de la prédiction au format JSON
         return jsonify({'prediction': str(predictions[0])})
 
-    # En cas d'erreur, retourne l'erreur au format JSON
     except Exception as e:
         return jsonify({'error': str(e)})
-
 
 # Exécution de l'application Flask si le script est exécuté directement
 if __name__ == '__main__':
--- Berthe_Pierrick_4_dossier_code_022024/scripts/dashboard.py
+++ Berthe_Pierrick_4_dossier_code_022024/scripts/dashboard.py
@@ -2,38 +2,45 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import mlflow
 import os
+import joblib
 
 st.title('Dashboard - Scoring crédit')
 
-DATA_URL = 'Data/train.csv'
+# Récupération du chemin absolu et du répertoire du fichier api.py
+chemin_racine = "C:\\Users\\pierr\\VSC_Projects\\Projet7_OCR_DataScientist"
 
-MODEL_URL_FLASK = ' ' # the link of the API flask
+# Chargement des données
+data_train_path = os.path.join(chemin_racine, "data", "cleaned", "application_train_cleaned.csv")
 
+# Chargement du modèle pré-entraîné
+MODEL_PATH = os.path.join(chemin_racine, 'mlflow_model', 'model.pkl')
+model = joblib.load(MODEL_PATH)
 
+MODEL_URL_FLASK = 'http://127.0.0.1:5000/predict'
+
+# Load data
 @st.cache_data
 def load_data(nom_fichier):
     """Charger les données à partir d'un fichier CSV."""
     return pd.read_csv(nom_fichier)
 
-# Décorateur pour mettre en cache le modèle
-@st.cache_resource
-def load_model(model_uri):
-    return mlflow.pyfunc.load_model(model_uri)
+data = load_data(data_train_path)
 
 def get_data_client(id_client):
     # filter on the choosen client 
-    # TO FILL
+    data_client = data[data['SK_ID_CURR'] == id_client]
     return data_client
 
-def request_prediction(model_uri, data):
+def request_prediction(MODEL_URL_FLASK, data):
     headers = {"Content-Type": "application/json"}
 
-    data_json = {'dataframe_split' : data.to_dict(orient='split')}
-    
-# TO FILL
-# use the mode POST on the MODEL URI  
-    response = requests.request(.....))
+    # Convertir le DataFrame en liste de dictionnaires
+    data_json = data.to_dict(orient='records')
+
+    # use the mode POST on the MODEL URI  
+    response = requests.post(MODEL_URL_FLASK, json=data_json)
 
     if response.status_code != 200:
         raise Exception(
@@ -41,26 +48,31 @@ def request_prediction(model_uri, data):
 
     return response.json()
 
-
 def main():
 
-    id_client = st.number_input('Id Client', value=0)
-    data = get_data_client(id_client)
+    id_client = st.number_input('Id Client', value=100002)
+    data_client = get_data_client(id_client)
     
-    st.dataframe(data)
+    st.dataframe(data_client)
     
     predict_btn = st.button('Prédire')
 
     if predict_btn:
-        response = request_prediction(MODEL_URL_FLASK, data)
+        response = request_prediction(MODEL_URL_FLASK, data_client)
         
-        st.write(response['prediction'])
+        if 'prediction' in response:
+            st.write(response['prediction'])
 
-        if int(response['prediction']) == '0' :
-            st.write('Accordé')
-        else :
-            st.write('refusé')
-
+            if int(response['prediction']) == 0 :
+                st.write('Accordé')
+            else :
+                st.write('Refusé')
+        else:
+            st.write("No prediction in the response")
 
 if __name__ == '__main__':
     main()
+
+
+# # pour appeler le dashboard : 
+# streamlit run Berthe_Pierrick_4_dossier_code_022024/scripts/dashboard.py
