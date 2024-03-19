1f81e87740af58066726be975edb248ae8608dd4
Pierrick BERTHE <pierrick.berthe@gmx.fr>
2024-03-19 12:52:37 +0100

    PB_19/03 : finalidation du feature importance dans dashboard en cours

--- Berthe_Pierrick_1_API_022024.py
+++ Berthe_Pierrick_1_API_022024.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 import os
+import shap
 
 # Création de l'application Flask
 app = Flask(__name__)
@@ -39,7 +40,6 @@ def predict():
     Prédit la sortie en fonction des données d'entrée en utilisant le modèle
     pré-entraîné. Les données d'entrée sont reçues au format JSON.
     """
-    print('request:')
     try:
         # Récupération des données au format JSON
         data = request.get_json()
@@ -47,11 +47,67 @@ def predict():
         # Convertir les données en DataFrame Pandas
         df = pd.DataFrame(data)
 
-        # Prédiction à l'aide du modèle pré-entraîné
-        predictions = model.predict(df)
+        # Prédictions à l'aide du modèle pré-entraîné
+        prediction = model.predict(df)[0]
+        prediction_proba = model.predict_proba(df)[0]
+        proba_class_1 = prediction_proba[1]
+
+        # Extraire le dernier estimateur du pipeline
+        final_estimator = model[-1]
+
+        # Expliquer les prédictions à l'aide de SHAP et son TreeExplainer
+        explainer = shap.TreeExplainer(final_estimator)
+        shap_values = explainer.shap_values(df)
+        shap_values_class_1 = shap_values[1][0]
+
+        # Convertir le tableau 1D en tableau 2D pour créer un DataFrame
+        shap_values_class_1_2d = shap_values_class_1.reshape(1, -1)
+
+        # DataFrame avec les valeurs SHAP et les noms de colonnes
+        shap_values_df = pd.DataFrame(
+            shap_values_class_1_2d,
+            columns=df.columns
+        )
+
+        # Nombre de features à afficher
+        nbr_feature=5
+
+        # Calculer la valeur absolue des valeurs SHAP
+        abs_shap_values = shap_values_df.abs()
+
+        # Obtenir les noms des nbr_feature ayant les valeurs SHAP les plus élevées
+        top_features = (
+            abs_shap_values.sum()
+            .sort_values(ascending=False)
+            .head(nbr_feature)
+            .index
+        )
+
+        # Sélectionner les top_features (arrondis)
+        client_data_subset = df[top_features].round(2)
+        shap_values_subset = shap_values_df[top_features].values.round(2)[0]
+
+        # Préparer la réponse
+        response = {
+            'prediction': {
+                'explainer' : explainer.expected_value[1],
+                'prediction': prediction.tolist(),
+                'probabilité': (proba_class_1 * 100).tolist()
+            },
+            'feature_importance': {
+                'shap_values': shap_values_class_1_2d[0].tolist(),
+                'feature_names': df.columns.tolist(),
+                'feature_values': df.values.tolist()[0],
+            },
+            'feature_importance_locale': {
+                'top_features': client_data_subset.columns.tolist(),
+                'shap_values_subset' : shap_values_subset.tolist(),
+                'client_data_subset': client_data_subset.values.tolist()[0]
+            }
+        }
 
         # Retour de la prédiction au format JSON
-        return jsonify({'prediction': str(predictions[0])})
+        return jsonify(response)
 
     except Exception as e:
         return jsonify({'error': str(e)})
--- Berthe_Pierrick_4_dossier_code_022024/scripts/dashboard.py
+++ Berthe_Pierrick_4_dossier_code_022024/scripts/dashboard.py
@@ -5,33 +5,78 @@ import requests
 import mlflow
 import os
 import joblib
+import shap
 
+# Titre de l'application
 st.title('Dashboard - Scoring crédit')
 
 # Récupération du chemin absolu et du répertoire du fichier api.py
 chemin_racine = "C:\\Users\\pierr\\VSC_Projects\\Projet7_OCR_DataScientist"
 
 # Chargement des données
-data_train_path = os.path.join(chemin_racine, "data", "cleaned", "application_train_cleaned.csv")
+data_train_path = os.path.join(
+    chemin_racine, "data", "cleaned", "application_train_cleaned.csv"
+)
 
 # Chargement du modèle pré-entraîné
 MODEL_PATH = os.path.join(chemin_racine, 'mlflow_model', 'model.pkl')
 model = joblib.load(MODEL_PATH)
 
+# URL du predict de l'API Flask
 MODEL_URL_FLASK = 'http://127.0.0.1:5000/predict'
 
-# Load data
+# Décorateur pour mettre en cache les données
 @st.cache_data
-def load_data(nom_fichier):
-    """Charger les données à partir d'un fichier CSV."""
-    return pd.read_csv(nom_fichier)
+def load_data(nom_fichier, _model):
+    """Charger les données à partir d'un fichier CSV et appliquer le prétraitement."""
+    # Lire les noms de colonnes
+    cols = pd.read_csv(nom_fichier, nrows=0).columns
 
-data = load_data(data_train_path)
+    # Supprimer la première colonne
+    cols = cols[1:]
 
-def get_data_client(id_client):
-    # filter on the choosen client 
-    data_client = data[data['SK_ID_CURR'] == id_client]
-    return data_client
+    # Lire le fichier CSV sans la première colonne
+    data = pd.read_csv(nom_fichier, usecols=cols)
+
+    # Isoler la colonne SK_ID_CURR et TARGET
+    sk_id_curr = data['SK_ID_CURR']
+    target = data['TARGET']
+
+    # Supprimer ces 2 cols
+    data = data.drop(columns=["TARGET", "SK_ID_CURR"])
+    
+    # Conserver les noms de colonnes
+    column_names = data.columns
+
+    # Appliquer les étapes de prétraitement (imputation + normalisation)
+    preprocessed_data = model.named_steps['preprocess'].transform(data)
+
+    # Recréer le DataFrame avec les noms de colonnes
+    preprocessed_data = pd.DataFrame(preprocessed_data, columns=column_names)
+
+    # Remettre col SK_ID_CURR
+    preprocessed_data['SK_ID_CURR'] = sk_id_curr
+    
+    return preprocessed_data
+
+# Chargement des données (avec preprocessing imputation du modèle)
+data = load_data(data_train_path, _model=model)
+
+
+def get_data_client(client_id):
+    # Obtenir les données pour le SK_ID_CURR sélectionné
+    client_data = data[data['SK_ID_CURR'] == client_id]
+    st.dataframe(client_data)
+
+    # Compter et afficher le nombre de valeurs manquantes
+    nan_nbr = client_data.isna().sum().sum()
+    st.write(f'Nombre NaN pour client {client_id} : {nan_nbr}')
+
+    # Supprimer la col SK_ID_CURR
+    client_data = client_data.drop(columns=['SK_ID_CURR'])
+
+    return client_data
+
 
 def request_prediction(MODEL_URL_FLASK, data):
     headers = {"Content-Type": "application/json"}
@@ -39,36 +84,112 @@ def request_prediction(MODEL_URL_FLASK, data):
     # Convertir le DataFrame en liste de dictionnaires
     data_json = data.to_dict(orient='records')
 
-    # use the mode POST on the MODEL URI  
+    # Faire une requête POST avec les données JSON
     response = requests.post(MODEL_URL_FLASK, json=data_json)
 
+    # Vérifier si la requête a réussi
     if response.status_code != 200:
         raise Exception(
-            "Request failed with status {}, {}".format(response.status_code, response.text))
+            "Request failed with status {}, {}".format(
+                response.status_code,
+                response.text
+            )
+        )
 
-    return response.json()
+    # Extraire le JSON de la réponse
+    response_json = response.json()
+
+    # Vérifier si 'error' est dans la réponse
+    if 'error' in response_json:
+        raise Exception(response_json['error'])
+
+    return response_json
+
+
+# def get_feature_importance_locale(response, client_data):
+
+#     # Nombre de features à afficher
+#     nbr_feature=5
+
+#     shap_values_df = response['feature_importance'].pop('feature_values', None)
+
+#     # # Convertir la liste en DataFrame
+#     # shap_values_df = pd.DataFrame(
+#     #     response["feature_importance"]["shap_values"],
+#     #     columns=response["feature_importance"]["feature_names"]
+#     # )
+
+#     # Calculer la valeur absolue des valeurs SHAP
+#     abs_shap_values = shap_values_df.abs()
+
+#     # Obtenir les noms des nbr_feature ayant les valeurs SHAP les plus élevées
+#     top_features = (
+#         abs_shap_values.sum()
+#         .sort_values(ascending=False)
+#         .head(nbr_feature)
+#         .index
+#     )
+
+#     # Sélectionner les top_features de X_test et shap_values_df (arrondis)
+#     client_data_subset = client_data[top_features].round(2)
+#     shap_values_subset = response["feature_importance"]["shap_values"][top_features].values.round(2)[0]
+
+#     return shap_values_subset, client_data_subset
+
 
 def main():
 
-    id_client = st.number_input('Id Client', value=100002)
-    data_client = get_data_client(id_client)
-    
-    st.dataframe(data_client)
-    
-    predict_btn = st.button('Prédire')
+    # Créer un widget de sélection pour choisir un SK_ID_CURR
+    client_id = st.selectbox(
+        'Choisissez un SK_ID_CURR',
+        data['SK_ID_CURR'].unique()
+    )
 
-    if predict_btn:
-        response = request_prediction(MODEL_URL_FLASK, data_client)
-        
-        if 'prediction' in response:
-            st.write(response['prediction'])
+    # Isoler les données du client sélectionné
+    client_data = get_data_client(client_id)
 
-            if int(response['prediction']) == 0 :
-                st.write('Accordé')
-            else :
-                st.write('Refusé')
+    # Ajouter un bouton pour déclencher la prédiction
+    if st.button('Calculer la prédiction'):
+
+        # Utiliser le modèle pour faire une prédiction
+        response = request_prediction(MODEL_URL_FLASK, client_data)
+
+        # Afficher la prédiction
+        if int(response['prediction']['prediction']) == 0:
+            st.markdown(
+                '<div style="background-color: #98FB98; padding: 10px;'
+                'border-radius: 5px; color: #000000;"'
+                '>Le prêt est accordé.</div>',
+                unsafe_allow_html=True
+            )
+
+        elif int(response['prediction']['prediction']) == 1:
+            st.markdown(
+                '<div style="background-color: #FF6347; padding: 10px;'
+                'border-radius: 5px; color: #000000;"'
+                '>Le prêt n\'est pas accordé.</div>',
+                unsafe_allow_html=True
+            )
+
         else:
-            st.write("No prediction in the response")
+            st.write("Pas de prédiction effectuée.")
+
+        # Afficher la probabilité
+        st.dataframe(response['prediction'])
+        st.dataframe(response['feature_importance'])
+        st.dataframe(response['feature_importance_locale'])
+
+        # #
+        # shap_values_subset, client_data_subset = get_feature_importance_locale(response, client_data)
+
+        # Afficher les valeurs SHAP pour les top_features
+        force_plot = shap.force_plot(
+            response['prediction']["explainer"],
+            response['feature_importance_locale']['shap_values_subset'],
+            response['feature_importance_locale']['client_data_subset'],
+            matplotlib=True
+        )
+
 
 if __name__ == '__main__':
     main()
