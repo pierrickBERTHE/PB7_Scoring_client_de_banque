6ea375f3dddb2e62a465b3fe8770ba66d4fac858
Pierrick BERTHE <pierrick.berthe@gmx.fr>
2024-03-27 12:22:48 +0100

    27/03: modif path data dans tests unitaires

--- Berthe_Pierrick_4_dossier_code_022024/tests_unitaires/test_unitaire_api.py
+++ Berthe_Pierrick_4_dossier_code_022024/tests_unitaires/test_unitaire_api.py
@@ -41,6 +41,18 @@ class TestFlaskApp(unittest.TestCase):
             os.getcwd(), "data", "cleaned", "application_train_cleaned.csv"
         )
 
+        # Choix du répertoire racine (local ou distant)
+        environment = os.getenv('ENVIRONMENT', 'distant')
+
+        if environment == 'local':
+            DATA_PATH = os.path.join(
+                os.getcwd(), "data", "cleaned", "application_train_cleaned.csv"
+            )
+        else:
+            DATA_PATH = os.path.join(
+                "..", "..", "data", "cleaned", "application_train_cleaned.csv"
+            )
+
         # Charge le fichier CSV dans un DataFrame pandas
         df = pd.read_csv(DATA_PATH)
 
